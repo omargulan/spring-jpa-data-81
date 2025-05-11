52ed64ab3b9445d6b43a9b92f5b5769554e07237
runtime-1 <runtimestudent2023@gmail.com>
2025-05-11 17:00:02 +0500

    11-05-2025

--- src/main/java/org/example/springjpadata81/controller/ProductController.java
+++ src/main/java/org/example/springjpadata81/controller/ProductController.java
@@ -3,6 +3,10 @@ package org.example.springjpadata81.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.springjpadata81.model.Product;
 import org.example.springjpadata81.repository.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -16,8 +20,22 @@ public class ProductController {
     private final ProductRepository productRepository;
 
     @GetMapping
-    public List<Product> findAll(){
-        return productRepository.findAll();
+//    public Page<Product> findAll(@RequestParam int page, @RequestParam int size){
+//        Pageable pageable = PageRequest.of(page, size);
+//        return productRepository.findAll(pageable);
+//    }
+    public List<Product> findAll(@RequestParam(defaultValue = "0") int page,
+                                 @RequestParam(defaultValue = "20") int size,
+                                 @RequestParam(defaultValue = "id") String field,
+                                 @RequestParam(defaultValue = "asc") String sortDirection,
+                                 @RequestParam(required = false) Long categoryId
+    ){
+        System.out.println(categoryId);
+        Sort sort  = Sort.by(Sort.Direction.fromString(sortDirection), field);
+
+        Pageable pageable = PageRequest.of(page, size, sort);
+        //return productRepository.findAll(pageable).getContent();
+        return productRepository.findByCategory_Id(categoryId, pageable).getContent();
     }
     @GetMapping("/{id}")
     public Product findById(@PathVariable long id){
--- src/main/java/org/example/springjpadata81/repository/ProductRepository.java
+++ src/main/java/org/example/springjpadata81/repository/ProductRepository.java
@@ -3,7 +3,10 @@ package org.example.springjpadata81.repository;
 import org.example.springjpadata81.model.Category;
 import org.example.springjpadata81.model.Product;
 import org.example.springjpadata81.model.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,4 +21,6 @@ public interface ProductRepository  extends JpaRepository<Product, Long> {
 
 
     List<Product> findByValues_Name(String valuesName);
+    @Query("select  p from Product p where :categoryId is null or  p.category.id = :categoryId")
+    Page<Product> findByCategory_Id(Long categoryId, Pageable pageable);
 }
